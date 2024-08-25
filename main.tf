77545693097298a06d8ddca3642a2cd3bf77ca66
Piyush Kumar Singh <sp9826086@gmail.com>
2024-08-25 17:37:51 +0530

    modify main.tf file

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-    ami = "ami-1234567890"
+    ami = "ami-1234567890123"
     instance_type = "t2.micro"
     vpc_security_group_ids = ["${aws_security_group.web.id}"]
 }
